9b962906554387f3a35010864e7dd1ea9739d541
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-12-10 12:31:46 +0800

    Add ProjectsSection component and update App layout
    Introduces a new ProjectsSection component and integrates it into the App layout alongside Header and HeroSection. The App now uses a styled container for consistent background and padding.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import Header from "./components/Header"
+import HeroSection from "./components/HeroSection"
+import ProjectsSection from "./components/ProjectsSection"
 
 function App() {
 
   return (
     <>
-      <Header />
+      <div className="bg-primary-fill min-h-screen">
+        <div className="px-4">
+
+          <Header />
+
+          <HeroSection />
+
+          <ProjectsSection />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function ProjectsSection() {
+    return (
+        <h1 className="text-white">i am the projects section</h1>
+    )
+}
