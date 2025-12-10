59837c9283cc034933fdd81cf9b91c83f336d6f9
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-12-10 12:31:23 +0800

    Redesign Header with Typography and menu icon
    Replaces the placeholder header with a styled header using the Typography component for the title and a FontAwesome hamburger menu icon. Improves layout and visual appearance for better user experience.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
+import Typography from "./ui/Typography"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 
 
 export default function Header() {
     return (
-        <h1>IM THE HEADER!!</h1>
+        <header>
+            <div className="py-6 flex justify-between items-center">
+                <Typography
+                    variant="mobileHeaderH1" className="text-neutral-offwhite"
+                >
+                    CHARLES D. VILLANUEVA
+                </Typography>
+
+                <FontAwesomeIcon
+                    icon={faBars}
+                    className="text-neutral-offwhite text-[25px] cursor-pointer"
+                />
+            </div>
+        </header>
+
     )
 }
